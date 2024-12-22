eabdc2f045ae638360c1c19de36fcb02ac584409
SOVANNARO <sovannaro066@gmail.com>
2024-12-22 21:48:27 +0700

    feat: ✨ add AlertDialog component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Button className="">Click me</Button>
+    <div className="flex flex-col justify-center items-center h-screen space-y-4">
+      <div className="flex space-x-4">
+        <Button variant="destructive">destructive</Button>
+        <Button variant="ghost">ghost</Button>
+        <Button variant="outline">outline</Button>
+        <Button variant="secondary">secondary</Button>
+      </div>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="default">Open Dialog</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
